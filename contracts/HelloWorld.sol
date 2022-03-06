3c0b23e3e8481b387c686eb6fa5fa58eaf9efef8
fasanya abiola <harbiola78@gmail.com>
2022-03-06 20:39:16 +0100

    commit

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,19 +3,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-    string public message;
     uint public messageCount = 0;
+    string public message;
 
-    constructor()  public {
-        message = "Hello World!";
+    constructor() public {
+        setMsg("Hello World");
     }
 
-    function get() public view returns(string memory) {
+    function getMsg() public view returns(string memory) {
         return message;
     }
 
-    function set(string memory _message) public {
-        messageCount++;
+    function setMsg(string memory _message) public {
         message = _message;
     }
+
 }
